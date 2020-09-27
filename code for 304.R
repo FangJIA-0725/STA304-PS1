d501aae4db9c76a33435a8d7470c6d95bbe16a3f
FangJIA-0725 <jiafang779@yahoo.com>
2020-09-27 23:53:32 +0000

    Third comment

--- code for 304.R
+++ code for 304.R
@@ -8,3 +8,16 @@ install.packages("devtools")
 blogdown::serve_site()
 # create a new post 
 
+blogdown::new_post("code and data supporting this analysis is available at here",
+                   kind = "",
+                   open = interactive(),
+                   author = getOption("blogdown.author"),
+                   categories = NULL,
+                   tags = NULL,
+                   date = Sys.Date(),
+                   file = NULL,
+                   slug = NULL,
+                   title_case = getOption("blogdown.title_case"),
+                   subdir = getOption("blogdown.subdir", "post"),
+                   ext = getOption("blogdown.ext", ".rmd"))
+
